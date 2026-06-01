460bb47fdeab6a042d240abe1e4e6aad5b2c4668
Mark Norman <mark@marknorman.com>
2026-06-01 14:12:04 -0500

    test(design-review): drop tautological resolver-function assertion

--- tests/archipelago/systems/test_operator_resolver_wiring.py
+++ tests/archipelago/systems/test_operator_resolver_wiring.py
@@ -1,12 +1,7 @@
 from __future__ import annotations
 
-from archipelago.agents.design_review.operator_resolver import operator_intervention_fn
 from archipelago.systems.pipeline import design, operator_resolver
 
 
 def test_resolver_wired_onto_loop() -> None:
     assert design.on_max_attempts_resolver is operator_resolver
-
-
-def test_resolver_uses_intervention_fn() -> None:
-    assert operator_resolver.function is operator_intervention_fn
